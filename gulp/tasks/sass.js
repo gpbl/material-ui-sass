bda43618c8b796b7ee5fc713101033b41dccdaef
Giampaolo Bellavite <gpberlin@gmail.com>
2014-12-03 14:13:25 +0100

    Fixes bug with autoprefixer and source maps

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,19 +1,23 @@
 'use strict';
 
 var gulp         = require('gulp');
+var filter       = require('gulp-filter');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-ruby-sass');
 
 var handleErrors = require('../utils/handleErrors');
-var config       = require('../config').sass;
+var config = require('../config').sass;
 
 gulp.task('sass', function () {
+	var filterCSS = filter('**/*.css');
 	gulp.src(config.src)
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-    .on('error', handleErrors)
-    .pipe(autoprefixer())
+		.on('error', handleErrors)
 		.pipe(sourcemaps.write())
+		.pipe(filterCSS)
+		.pipe(autoprefixer())
+		.pipe(filterCSS.restore())
 		.pipe(gulp.dest(config.dest));
 });
