7044eb7d62da81b98ba71647dedd618990e546e4
Giampaolo Bellavite <gpberlin@gmail.com>
2014-12-03 14:13:06 +0100

    Add a log of what it is watched

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var src = require('../config').watch;
 gulp.task('watch', function () {
+	gutil.log(gutil.colors.grey('Watching ' + src + '...'));
 	gulp.watch(src, ['sass']);
 });
