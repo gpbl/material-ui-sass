d20923a619fd8044d940cd2a8239f65c542a710f
gpbl <io@gpbl.org>
2015-03-09 21:59:01 +0100

    Update tasks to work with the less submodule

--- gulp/config.js
+++ gulp/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	watch: ['material-ui/**/*.scss', 'docs/**/*.scss'],
 	sass: {
 		src: './docs/*.scss',
-		dest: './node_modules/material-ui/docs/build'
+		dest: './less/docs/build'
 	}
 };
--- gulp/tasks/gulp-docs.js
+++ gulp/tasks/gulp-docs.js
@@ -4,7 +4,7 @@ var run = require('gulp-run');
 // Run gulp for the docs site
 gulp.task('gulp-docs', function () {
   var cmd = new run.Command('gulp', {
-    cwd: './node_modules/material-ui/docs'
+    cwd: './less/docs'
   });
   cmd.exec();
 });
